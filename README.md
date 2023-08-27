963c3388ccefd0e573b011508cf4822a728f7fee
Deej Potter <deejpotter@gmail.com>
2023-08-27 16:11:39 +1000

    Updated the interface to use Flask-Bootstrap and improved the dev experience.

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Maker Store AI
 
 A chatbot which can provide detailed technical information about Maker Store products and machines.
+It is built using Python and Flask, and deployed on Heroku.
+
+## Note
+
+This is a work in progress. This is for testing purposes only. It will almost certainly not work as intended and tell you incorrect information. Always double check with a Maker Store staff member.
 
 ## Getting Started
 
 To get started, just clone this repository and run the following command:
 
 ```bash
-python3 main.py
+flask run
 ```
+
+It will require your own key for the openai api to be set in the .env
--- app.py
+++ app.py
@@ -1,34 +1,38 @@
-# This is the main file that runs the Flask app.
-# It starts the Flask app and defines the routes for the app.
+# This is the main file that runs the Flask app. It starts the Flask app and defines the routes for the app.
 
+# Import the Flask class from the flask package. Flask is a web framework for Python which is simple and lightweight.
 from flask import Flask, render_template, request, jsonify
+# Import the load_dotenv function from the dotenv package
 from dotenv import load_dotenv
+# Import the os module to access environment variables
 import os
+# Import the Bootstrap class from the flask_bootstrap package
+from flask_bootstrap import Bootstrap
+# Import the ChatHistory and BotResponse classes for handling the conversation history and bot response
 from chat_history import ChatHistory
 from bot_response import BotResponse
 
 # Initialize the Flask app
 app = Flask(__name__)
-
-# Load environment variables
+# Load environment variables from the .env file
 load_dotenv()
-
+# Initialize Bootstrap using the Flask app to use the Bootstrap CSS framework
+Bootstrap(app)
 # Load the conversation history
 chat_history = ChatHistory()
-
-# Initialize BotResponse
+# Initialize the BotResponse class, so we can access the methods of the class
 bot_response_instance = BotResponse(os.getenv("OPENAI_API_KEY"))
-
-# Load existing conversation history
+# Load existing conversation history so the bot can continue the conversation from where it left off
 conversation_history = chat_history.load_conversation_history()
 
 
+# The home route. Renders the home page which contains the chatbot.
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-# This route handles the user input to ask a question to the bot.
+# This route handles the user input to ask a question to the bot. It is called when the user clicks the "Ask" button.
 @app.route('/ask', methods=['POST'])
 def ask():
     # Get the user message from the request, which is entered by the user in the text input field
