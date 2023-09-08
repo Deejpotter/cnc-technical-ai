49772175c8aa12100d6ac692a3e836310fd914db
Deej Potter <deejpotter@gmail.com>
2023-09-08 20:02:05 +1000

    Implemented specific error handling for the flask app."

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Import necessary modules
 from flask import Flask, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
+import logging  # Import the logging module
 
 from chat_engine import ChatEngine  # Importing the ChatEngine class
 
@@ -10,8 +11,10 @@ app = Flask(__name__)
 Bootstrap(app)
 
 # Create an instance of the ChatEngine class
-# This will run the __init__ method in the ChatEngine class
 chat_engine = ChatEngine()
+
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.DEBUG)  # Configure basic logging to a file
 
 
 # Define the home route
@@ -29,13 +32,21 @@ def ask() -> object:
         user_message: str = request.form['user_message']
 
         # Call the process_user_input method from the chat_engine object
-        # This method will handle the user input and generate a bot response
         bot_response: object = chat_engine.process_user_input(user_message)
 
         return jsonify({'bot_response': bot_response})
 
+    except KeyError:
+        logging.error("KeyError occurred")  # Log the error
+        return jsonify({'error': 'KeyError: Missing key in request'}), 400  # Return a JSON response for KeyError
+
+    except ValueError:
+        logging.error("ValueError occurred")  # Log the error
+        return jsonify({'error': 'ValueError: Invalid value in request'}), 400  # Return a JSON response for ValueError
+
     except Exception as e:
-        return jsonify({'error': str(e)})
+        logging.error(f"An unexpected error occurred: {str(e)}")  # Log the error
+        return jsonify({'error': str(e)}), 500  # Return a JSON response for general exceptions
 
 
 # Run the Flask app
